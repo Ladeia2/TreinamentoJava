1222147e56d71fa29f063f458bcaad2c16fa1d06
ladeia2 <edimarsoares2@gmail.com>
2020-06-17 22:25:47 -0300

    Treinamento java

---
+++ meuscript.js
@@ -0,0 +1,9 @@
+
+ let numero1 = window.prompt("Digite um numero");
+ let numero2 = window.prompt("Digite um numero");
+
+ if(numero1> numero2) {
+     alert(" 0 Número " + numero1  +  " É o maior ")
+ }else{
+     alert(" 0 Número " + numero2  +  " É o maior ")}
+
